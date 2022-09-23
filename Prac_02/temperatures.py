2eda35633b643aa42b9144fc0ca3954d5e37cdbf
CobeySmith <cobeyleigh02@gmail.com>
2022-09-23 11:07:19 +1000

    Temperatures prac with functions

---
+++ Prac_02/temperatures.py
@@ -0,0 +1,39 @@
+"""Temperatures"""
+
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            celsius = float(input("Celsius: "))
+            convert_to_fahrenheit(celsius)
+        elif choice == "F":
+            fahrenheit = float(input("Fahrenheit: "))
+            convert_to_celsius(fahrenheit)
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
+
+
+def convert_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    print(f"Result: {fahrenheit:.2f} F")
+
+
+def convert_to_celsius(fahrenheit):
+    celsius = 5 / 9 * (fahrenheit - 32)
+    print(f"Result: {celsius:.2f} C")
+
+
+
+
+
+main()
