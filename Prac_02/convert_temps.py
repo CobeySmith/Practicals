13d3cfff4237ee646d7c686827660b0c202ddebc
CobeySmith <cobeyleigh02@gmail.com>
2022-09-27 09:56:45 +1000

    Version that doesn't output errors

--- Prac_02/convert_temps.py
+++ Prac_02/convert_temps.py
@@ -1,35 +1,20 @@
 """Covert Temperatures"""
 
-input_file = "temps_input.txt"
 
-
-# def main():
-#     fahrenheit_values = open(input_file, "r")
-#     fahrenheit_values.read())
-#     save_results_to_file("temps_output.txt", fahrenheit_values)
-#
-#
-# def get_fahrenheit_values():
-#     input_file = open(input_file, "r")
-#     fahrenheit_values.read())
-
-def save_results_to_file(file_name, fahrenheit_values):
-    """Save scores and their results to a file"""
-    output_file = open(file_name, "w")
-    for fahrenheit_value in fahrenheit_values:
+def main():
+    infile = open("temps_input.txt", "r")
+    outfile = open("results.txt", "w")
+    for line in infile:
+        fahrenheit_value = float(line.strip())
         celsius_value = convert_to_celsius(fahrenheit_value)
-        output_file.write(f"{celsius_value}\n")
-    output_file.close()
+        outfile.write(f"{celsius_value}\n")
+    infile.close()
+    outfile.close()
 
 
-def convert_to_fahrenheit(celsius):
-    """covert celsius to fahrenheit"""
-    return celsius * 9.0 / 5 + 32
-
-
-def convert_to_celsius(fahrenheit_values):
+def convert_to_celsius(fahrenheit_value):
     """convert fahrenheit to celsius"""
-    return 5 / 9 * (fahrenheit_values - 32)
+    return 5 / 9 * (fahrenheit_value - 32)
 
 
 main()
