46d542dd80437c990709fa016a98018a34fa3a29
CobeySmith <cobeyleigh02@gmail.com>
2022-09-27 10:32:56 +1000

    Initial commit

---
+++ Prac_03/randoms.py
@@ -0,0 +1,15 @@
+"""Random"""
+
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+"""
+1. 5
+2. 3 was the smallest, 9 was the largest. No 4
+3. 2.5 was the smallest, 5.5 was the largest.
+"""
+
+print(random.randint(1, 100))
