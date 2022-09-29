44e4686209fce00acc334de9a1914c795233ed4c
CobeySmith <cobeyleigh02@gmail.com>
2022-09-29 21:11:08 +1000

    Change open to with

--- Prac_03/capitalist_conrad.py
+++ Prac_03/capitalist_conrad.py
@@ -23,15 +23,9 @@ with open(OUTPUT_FILE, 'w') as out_file:
     while MIN_PRICE <= price <= MAX_PRICE:
         price_change = 0
         count += 1
-        # generate a random integer of 1 or 2
-        # if it's 1, the price increases, otherwise it decreases
         if random.randint(1, 2) == 1:
-            # generate a random floating-point number
-            # between 0 and MAX_INCREASE
             price_change = random.uniform(0, MAX_INCREASE)
         else:
-            # generate a random floating-point number
-            # between negative MAX_DECREASE and 0
             price_change = random.uniform(-MAX_DECREASE, 0)
 
         price *= (1 + price_change)
