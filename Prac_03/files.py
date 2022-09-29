427b4dad7fae2c32d5cf110a683e673b8dd57c03
CobeySmith <cobeyleigh02@gmail.com>
2022-09-29 18:05:26 +1000

    Add main, function, change open to with

--- Prac_03/files.py
+++ Prac_03/files.py
@@ -1,38 +1,45 @@
 """Files"""
 
-# Question 1
-# name = input("Name: ")
-# outfile = open("name.txt", "w")
-# outfile.write(name)
-# outfile.close()
-#
-# # Question 2
-# infile = open("name.txt", "r")
-# name = infile.readline()
-# print(f"Your name is {name}")
-# infile.close()
-#
-# # Question 3
-# infile = open("numbers.txt", "r")
-# first_number = int(infile.readline())
-# second_number = int(infile.readline())
-# infile.close()
-# total = first_number + second_number
-# print(f"The total is: {total}")
 
-# Question 4
-infile = open("numbers.txt", "r")
-error_line_numbers = []
-total = 0
-for line_number, line in enumerate(infile, 1):
+def main():
+    # Question 1
+    name = input("Name: ")
+    with open("name.txt", "w") as outfile:
+        outfile.write(name)
+
+    # Question 2
+    with open("name.txt", "r") as infile:
+        name = infile.readline().strip()
+        print(f"Your name is {name}")
+
+    # Question 3
+    with open("numbers.txt", "r") as infile:
+        first_number = get_valid_number(infile)
+        second_number = get_valid_number(infile)
+    total = first_number + second_number
+    print(f"The total is: {total}")
+
+    # Question 4
+    with open("numbers.txt", "r") as infile:
+        error_line_numbers = []
+        total = 0
+        for line_number, line in enumerate(infile, 1):
+            try:
+                number = float(line)
+                total += number
+            except ValueError:
+                error_line_numbers.append(line_number)
+    error_line_numbers = ", ".join([str(number) for number in error_line_numbers])
+    print(f"These lines have errors: {error_line_numbers}")
+    print(f"The total is: {total}")
+
+
+def get_valid_number(infile):
     try:
-        number = float(line)
-        total += number
-    except ValueError as error:
-        error_line_numbers.append(line_number)
-infile.close()
-error_line_numbers = ", ".join([str(number) for number in error_line_numbers])
-print(f"These lines have errors: {error_line_numbers}")
-print(f"The total is: {total}")
+        number = float(infile.readline())
+        return number
+    except ValueError:
+        return 0
 
 
+main()
