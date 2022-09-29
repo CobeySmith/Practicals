f6540a71e9096b0ae6fcebf57129220430b61617
CobeySmith <cobeyleigh02@gmail.com>
2022-09-29 21:09:00 +1000

    Change open to with

--- Prac_03/capitalist_conrad.py
+++ Prac_03/capitalist_conrad.py
@@ -17,23 +17,22 @@ INITIAL_PRICE = 10.0
 OUTPUT_FILE = "capitalist_output.txt"
 
 price = INITIAL_PRICE
-out_file = open(OUTPUT_FILE, 'w')
-print(f"Starting price: ${price:,.2f}", file=out_file)
-count = 0
-while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    count += 1
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
-    if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
-        price_change = random.uniform(0, MAX_INCREASE)
-    else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
+with open(OUTPUT_FILE, 'w') as out_file:
+    print(f"Starting price: ${price:,.2f}", file=out_file)
+    count = 0
+    while MIN_PRICE <= price <= MAX_PRICE:
+        price_change = 0
+        count += 1
+        # generate a random integer of 1 or 2
+        # if it's 1, the price increases, otherwise it decreases
+        if random.randint(1, 2) == 1:
+            # generate a random floating-point number
+            # between 0 and MAX_INCREASE
+            price_change = random.uniform(0, MAX_INCREASE)
+        else:
+            # generate a random floating-point number
+            # between negative MAX_DECREASE and 0
+            price_change = random.uniform(-MAX_DECREASE, 0)
 
-    price *= (1 + price_change)
-    print(f"On Day {count} price is: ${price:,.2f}", file=out_file)
-out_file.close()
+        price *= (1 + price_change)
+        print(f"On Day {count} price is: ${price:,.2f}", file=out_file)
