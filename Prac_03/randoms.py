d1901e664545ced9b4313b0507dd68216a342c81
CobeySmith <cobeyleigh02@gmail.com>
2022-09-29 21:18:41 +1000

    Change answers

--- Prac_03/randoms.py
+++ Prac_03/randoms.py
@@ -7,8 +7,8 @@ print(random.randrange(3, 10, 2))  # line 2
 print(random.uniform(2.5, 5.5))  # line 3
 
 """
-1. 5
-2. 3 was the smallest, 9 was the largest. No 4
+1. 5 was the smallest, 20 was the largest
+2. 3 was the smallest, 9 was the largest. Not able to produce 4.
 3. 2.5 was the smallest, 5.5 was the largest.
 """
 
