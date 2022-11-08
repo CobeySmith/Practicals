3815a74e5b7c710015707e28826d1d30d04d2f96
CobeySmith <113513206+CobeySmith@users.noreply.github.com>
2022-11-08 15:13:33 +1000

    Fix datetime section and complete save_data and load_data functions.

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,7 @@ Prac 07 - Project Management
 Estimate: 45 mins
 Actual:
 """
+import datetime
 
 from project import Project
 import datetime
@@ -11,17 +12,19 @@ FILENAME = "projects.txt"
 
 
 def main():
+    title = load_title(FILENAME)
     projects = load_data(FILENAME)
+    # for project in projects:
+    #     print(project)
     menu_choice = input("Menu Choice: ").upper()
     while menu_choice != "Q":
         if menu_choice == "L":
-            print(projects)
             filename = input("Filename: ")
             projects = load_data(filename)
         elif menu_choice == "S":
-            save_data(projects)
+            save_data(projects, title)
         elif menu_choice == "D":
-            display_projects(projects)
+            display_sorted_projects(projects)
         elif menu_choice == "F":
             print("Filter")
         elif menu_choice == "A":
@@ -34,11 +37,19 @@ def main():
     print("Thanks for coming")
 
 
-def save_data(projects):
+def load_title(filename):
+    with open(filename, "r") as in_file:
+        title = in_file.readline().strip().split("\t")
+    return title
+
+
+def save_data(projects, title):
     out_file = input("Save file name: ")
     with open(out_file, "w") as out_file:
+        print("\t".join(title), file=out_file)
         for project in projects:
-            print(f"{project}", file=out_file, end="\n")
+            print(f"{project.name}\t{project.start_date}\t{project.priority}\t{project.estimate}\t{project.completion}",
+                  file=out_file, end="\n")
 
 
 def load_data(filename):
@@ -49,13 +60,14 @@ def load_data(filename):
         for line in in_file:
             parts = line.strip().split("\t")
             date_string = parts[1]
-            date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
-            project = Project(parts[0], date, int(parts[2]), float(parts[3]), int(parts[4]))
+            start_date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+            project = Project(parts[0], f"{start_date.day}/{start_date.month}/{start_date.year}",
+                              int(parts[2]), float(parts[3]), int(parts[4]))
             projects.append(project)
     return projects
 
 
-def display_projects(projects):
+def display_sorted_projects(projects):
     completed_projects = [project for project in projects if project.is_complete()]
     incomplete_projects = [project for project in projects if not project.is_complete()]
     print("Incomplete Projects: ")
